b5befc9c4b043a24df4e2c31940be5b296ce25a7
diki <adiki436@gmail.com>
2026-04-06 08:23:31 +0700

    Answer: Logic Exercise 2

---
+++ Logic Exercise 2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Masukkan angka: ");
+int n = int.Parse(Console.ReadLine()!);
+
+for(int i = 1; i <= n; i++)
+{
+    string output = "";
+    
+    if(i % 3 == 0) output += "foo";
+    
+    if(i % 5 == 0) output += "bar";
+    
+    if(i % 7 == 0) output += "jazz";
+    
+    if(output == "") output = i.ToString();
+    
+    Console.Write(output);
+    
+    if(i < n) Console.Write(", ");
+}
+
+Console.WriteLine();
